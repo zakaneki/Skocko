d5995f1f6af3262f22c1f2f8b171fd057baf195b
zakaneki <zakicandrija12@gmail.com>
2025-06-12 12:52:27 +0200

    Refactor variable names for clarity in Test.java

--- src/slagalica/Test.java
+++ src/slagalica/Test.java
@@ -19,18 +19,18 @@ public class Test {
 		System.out.println("x - number of guessed symbols in the same position as in your combination");
 		System.out.println("y - number of guessed symbols in a different position than in your combination but still contained by your combination");
 		while(true) {
-			List<String[]> lista = new ArrayList<>();
-			setup(lista);
+			List<String[]> list = new ArrayList<>();
+			setup(list);
 
-			String tacno[] = lista.get(new Random().nextInt(lista.size()));
+			String correct[] = list.get(new Random().nextInt(list.size()));
 			
-			while(lista.size() > 1) {
-				guess(lista, scanner, tacno);
+			while(list.size() > 1) {
+				guess(list, scanner, correct);
 			}
-			if(lista.size() != 1) {
+			if(list.size() != 1) {
 				System.out.println("Some of your answers were impossible, therefore I cannot guess the combination you're thinking of");
 			}
-			else  System.out.println("Your combination is " + lista.get(0)[0] + " " + lista.get(0)[1] + " " + lista.get(0)[2] + " " + lista.get(0)[3]);
+			else  System.out.println("Your combination is " + list.get(0)[0] + " " + list.get(0)[1] + " " + list.get(0)[2] + " " + list.get(0)[3]);
 			System.out.println("Would you like to go again? 0 - no, 1 - yes ");
 			if(scanner.nextInt() == 0) break;
 		}
@@ -39,72 +39,72 @@ public class Test {
 		scanner.close();
 	}
 	
-	private static void guess(List<String[]> lista, Scanner scanner, String[] tacno) {
-		String[] s = pick(lista);
+	private static void guess(List<String[]> list, Scanner scanner, String[] correct) {
+		String[] s = pick(list);
 		
 
         System.out.print(s[0] + " " + s[1] + " " + s[2] + " " + s[3] + " : ");
-        //int odg[] = compare(s, tacno);
+        //int odg[] = compare(s, correct);
         //System.out.println(odg[0] + " " + odg[1]);
-        int odg[] = new int[2];
-        odg[0] = scanner.nextInt();
-        odg[1] = scanner.nextInt();
-        eliminate(lista, odg, s);
+        int ans[] = new int[2];
+        ans[0] = scanner.nextInt();
+        ans[1] = scanner.nextInt();
+        eliminate(list, ans, s);
 	}
 
-	private static void eliminate(List<String[]> lista, int[] odg, String[] s) {
-		Iterator<String[]> iterator = lista.iterator();
+	private static void eliminate(List<String[]> list, int[] ans, String[] s) {
+		Iterator<String[]> iterator = list.iterator();
 	    
 	    while (iterator.hasNext()) {
 	        String[] elem = iterator.next();
-	        if (!Arrays.equals(compare(elem, s), odg)) {
+	        if (!Arrays.equals(compare(elem, s), ans)) {
 	            iterator.remove(); // Use the iterator to remove the element
 	        }
 	    }
 		
 	}
 
-	private static String[] pick(List<String[]> lista) {
-		int scores[] = new int[lista.size()];
+	private static String[] pick(List<String[]> list) {
+		int scores[] = new int[list.size()];
 		for(int i = 0; i < scores.length; i++) scores[i] = 0;
-		for(int i = 0; i < lista.size(); i++) {
-			scores[i] = eliminationCount(lista.get(i), lista);
+		for(int i = 0; i < list.size(); i++) {
+			scores[i] = eliminationCount(list.get(i), list);
 		}
-		List<Integer> izbori = new ArrayList<>();
-		izbori.add(0);
+		List<Integer> choices = new ArrayList<>();
+		choices.add(0);
 		for(int i = 0; i < scores.length; i++) {
-			if(scores[izbori.get(0)] < scores[i]) {
-				izbori.clear();
-				izbori.add(i);
+			if(scores[choices.get(0)] < scores[i]) {
+				choices.clear();
+				choices.add(i);
 			}
-			else if(scores[izbori.get(0)] == scores[i]) {
-				izbori.add(i);
+			else if(scores[choices.get(0)] == scores[i]) {
+				choices.add(i);
 			}
 			
 		}
-		return lista.get(izbori.get(new Random().nextInt(izbori.size())));
+		return list.get(choices.get(new Random().nextInt(choices.size())));
 		
 	}
 
-	private static int eliminationCount(String[] strings, List<String[]> lista) { 
+	private static int eliminationCount(String[] strings, List<String[]> list) { 
 		int x = 0;
 		List<int[]> answers = generateAnswers(4);
-		for(int[] a : answers) x += tryAnswerAndCombination(a, strings, lista);
+		for(int[] a : answers) x += tryAnswerAndCombination(a, strings, list);
 		return x;
 	}
 
-	private static int tryAnswerAndCombination(int[] a, String[] strings, List<String[]> lista) {
+	private static int tryAnswerAndCombination(int[] a, String[] strings, List<String[]> list) {
 		int x = 0;
-		for(String[] s : lista) {
+		for(String[] s : list) {
 			if(!Arrays.equals(s, strings)) {
 				int cmp[] = compare(s, strings);
-				if((cmp[0] != a[0] || cmp[1] != a[1]) && !impossibleAnswer(strings, lista, a)) x++;
+				if((cmp[0] != a[0] || cmp[1] != a[1]) && !impossibleAnswer(strings, list, a)) x++;
 			}
 		}
 		return x;
 	}
 
-	private static boolean impossibleAnswer(String[] s, List<String[]> lista, int[] cmp) {
+	private static boolean impossibleAnswer(String[] s, List<String[]> list, int[] cmp) {
 		if(cmp[0] == 3 && cmp[1] == 1) return true;
 		else if(cmp[0] == 2 && cmp[1] == 2) return check(0, s) || check(1, s);
 		else if(cmp[0] == 2 && cmp[1] == 1) return check(0, s);
